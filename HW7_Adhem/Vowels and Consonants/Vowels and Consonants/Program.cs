03ab2d5c73a1d5f705115aae505e805bacaaa94b
Addikins <addikinsplays@gmail.com>
2020-04-07 18:05:12 -0700

    Add Homework 7 setup

---
+++ HW7_Adhem/Vowels and Consonants/Vowels and Consonants/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowels_and_Consonants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            char[] GetUserInput()
+            {
+                bool isValidInput = true;
+                char[] userInput;
+
+                do
+                {
+                    Console.Write("Please enter a word: ");
+                    userInput = Console.ReadLine().ToCharArray();
+
+                    foreach (char letter in userInput)
+                    {
+                        if (char.IsLetter(letter))
+                        {
+                            continue;
+                        }
+                        isValidInput = false;
+                        Console.WriteLine("Word can only contain letters. No spaces, numbers, or symbols are allowed");
+                    }
+
+                }
+                while (!isValidInput);
+
+                Console.WriteLine("Thank you!");
+                return userInput;
+            }
+        }
+    }
+}
